b8d11b41c0748a8aa65946eeb47d2d7491d0b61e
Sahmie Nwoko <sahmienwoko@gmail.com>
2020-02-21 19:48:05 +0100

    solve calculator challenge

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(arr)
+  result = 0
+  arr.each {|a| result += a }
+  result 
+end
+
+def multiply(num1, num2, *num3)
+  result = num1 * num2
+  num3.each { |x| result *= x }
+  result
+end
+
+def power(num1, num2)
+  num1**num2
+end
