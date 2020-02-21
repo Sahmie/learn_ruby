8d6f7061cbed1b1f4a12be96bbcbec5d86638733
Sahmie Nwoko <sahmienwoko@gmail.com>
2020-02-21 20:23:18 +0100

    solve simon_says Challenge

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
 #write your code here
+def echo(word)
+  word
+end
+
+def shout(word)
+  word.upcase
+end
+
+def repeat(word, num = 2)
+  words = word + ' '
+  ( words * num ).strip
+end
+
+
+def start_of_word(word, num)
+  num -= 1
+  return word[0..num] if num >= 1
+  word[num]
+end
+
+#longer version of the above solution
+# def start_of_word(word, num)
+#   num -= 1
+#   if num >= 1
+#     return word[0..num]
+#   else
+#     return word[num]
+#   end
+# end
+
+def first_word(word)
+  word.split(' ')[0]
+end
+
+# def titleize()
